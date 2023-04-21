7ff8f6758e879797d47c1c5d12fb18b06311e51e
Charith J De Silva <charithjdesilva@gmail.com>
2023-04-22 00:41:38 +0530

    display name under the face

--- AttendanceMarkingSystem.py
+++ AttendanceMarkingSystem.py
@@ -65,6 +65,12 @@ while True:     # to get each frame one by one
             name = imageNames[matchIndex]
             print(name)
 
-        # display a bounding box around that person and display their name
-        cv2.imshow('webcam', img)   # show the original image
-        cv2.waitKey(1)
+            # display a bounding box around that person and display their name
+            y1, x2, y2, x1 = faceLocation    # we have the location of the face in the imageSmall, we destruct the list
+            y1, x2, y2, x1 = y1*4, x2*4, y2*4, x1*4    # location of the face in the original image
+            cv2.rectangle(img, (x1, y1), (x2, y2), (255,255,0), 2)
+            cv2.rectangle(img, (x1, y2-35), (x2, y2), (255,255,0), 2)  # rectangle to display name
+            cv2.putText(img, name, (x1+6, y2-6), cv2.FONT_HERSHEY_COMPLEX, 1, (255,255,255), 2)
+
+    cv2.imshow('webcam', img)   # show the original image
+    cv2.waitKey(1)
